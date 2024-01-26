81be4129ab58ea196c9530b82079b7ed5f36ab3d
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-01-26 17:28:46 +0200

    CreareServerBackend commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
   return (
     <header
       style={{
-        backgroundColor: '#3182ce',
+        backgroundColor: '#a2b5d5',
         position: 'fixed',
         top: 0,
         width: '100%',
